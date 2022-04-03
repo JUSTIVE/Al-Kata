5e8dc51f1b7238b977fb86f39701f392763677f0
DosangGu <ddos0418@gmail.com>
2022-04-03 12:27:55 +0900

    3회차 도상 1316 추가

---
+++ 3회차/1316/ds.py
@@ -0,0 +1,53 @@
+import sys
+from typing import List
+
+def main():
+    N = int(sys.stdin.readline().split()[0])
+    words = list(map(lambda x: sys.stdin.readline().split()[0], range(N)))
+
+    def JoinChars(word: str) -> str:
+        word_list = list(word)
+
+        def DelChar(idx: int, word_list: List[str]) -> List[str]:
+            if idx == len(word_list)-1:
+                pass
+
+            elif word_list[idx] == word_list[idx+1]:
+                del word_list[idx+1]
+                word_list = DelChar(idx, word_list)
+
+            elif word_list[idx] != word_list[idx+1]:
+                word_list = DelChar(idx+1, word_list)
+
+            return word_list
+
+        word_list_joined = DelChar(0, word_list)
+        return ''.join(word_list_joined)
+
+    JoinedWords = list(map(JoinChars, words))
+
+    def IsGroup(Name:str) -> bool:
+        word_list = list(Name)
+        
+        def CharExist(Name: str, char: str, idx: int) -> bool:
+            if Name.find(char, idx+1) > 0:
+                return True
+            else:
+                return False
+        
+        isgroup = True
+
+        for idx, word in enumerate(word_list):
+            if CharExist(Name, word, idx):
+                isgroup = False
+                break
+
+        return isgroup
+
+    print(len(list(filter(IsGroup, JoinedWords))))
+
+
+
+
+if __name__ == '__main__':
+    main()
