b1ffab37df1fbc76bc5e3f202adad3d55ececa65
DosangGu <ddos0418@gmail.com>
2022-04-14 18:06:54 +0900

    ds 5회차 업데이트

--- 5회차/1003/ds.py
+++ 5회차/1003/ds.py
@@ -1,22 +1,26 @@
 import sys
 from typing import Dict, List
 
-N = int(sys.stdin.readline().split()[0])
-numbers = list(map(lambda x: sys.stdin.readline().split()[0], range(N)))
-numbers = list(map(int,numbers))
-MemoFibo = {
-    0:[1,0],
-    1:[0,1]
-    }
+def main():
+    N = int(sys.stdin.readline().split()[0])
+    numbers = list(map(lambda x: sys.stdin.readline().split()[0], range(N)))
+    numbers = list(map(int,numbers))
+    MemoFibo = {
+        0:[1,0],
+        1:[0,1]
+        }
 
-def fibonacci(n: int) -> Dict[int, List[int]]:
-    try:
-        result = MemoFibo[n]
-    except KeyError:
-        result = [fibonacci(n-1)[i] + fibonacci(n-2)[i] for i in range(2)]
-        MemoFibo[n] = result
-    return result
+    def fibonacci(n: int) -> Dict[int, List[int]]:
+        try:
+            result = MemoFibo[n]
+        except KeyError:
+            result = [fibonacci(n-1)[i] + fibonacci(n-2)[i] for i in range(2)]
+            MemoFibo[n] = result
+        return result
 
-fibonacci(max(numbers))
+    fibonacci(max(numbers))
 
-[print(MemoFibo[i][0], MemoFibo[i][1]) for i in numbers]
+    [print(MemoFibo[i][0], MemoFibo[i][1]) for i in numbers]
+
+if __name__ == '__main__':
+    main()
