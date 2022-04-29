470c698f1b51e81d94192e9bc2d5fda8af71ba32
DosangGu <ddos0418@gmail.com>
2022-04-29 11:08:04 +0900

    dosang 1927

---
+++ 10회차/1927/ds.py
@@ -0,0 +1,18 @@
+import sys
+from typing import List
+
+def main():
+    N = int(sys.stdin.readline()[0])
+    heap:List[int] = []
+    heap.append(0)
+    for i in range(N):
+        act()
+
+def act():
+    x = int(sys.stdin.readline()[0])
+
+    if 
+    pass
+
+if __name__ == '__main__':
+    main()
